ba3ba5d52091bc19d46a4972d6a14e616fbfa9cd
AAAA-CS <138863576+AAAA-CS@users.noreply.github.com>
2024-07-07 00:26:51 +0800

    Update root_paths.py

--- src/datasets/root_paths.py
+++ src/datasets/root_paths.py
@@ -1,7 +1,6 @@
 DATA_ROOTS = {
     # 'potsdam': 'data/potsdam/train_RGB',
-    'potsdam': r'E:\Gloriah\Maker\viewmaker-main\data\potsdam\train_RGB',
-    # 'loveDa': 'data/loveDA_selfsup_processed',
+    'potsdam': '\viewmaker-main\data\potsdam\train_RGB',
     'vaihingen': 'data/vaihingen_selfsup_256_64',
     'loveDa': 'data/loveDa/loveDA_selfsup',
     'cifar10': 'data/cifar10',
